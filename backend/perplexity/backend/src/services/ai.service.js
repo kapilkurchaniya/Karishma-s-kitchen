340551898a67cf26827f55552b003b77d9e00112
kapil_k15 <kapilkurchaniya98@gmail.com>
2026-05-03 18:06:30 +0530

    Prune repository to keep only backend/krihi-mitra

--- backend/perplexity/backend/src/services/ai.service.js
+++
@@ -1,8 +0,0 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-
-
-const model = new ChatGoogleGenerativeAI({
-  model: "gemini-2.5-flash-lite",
-  apiKey: process.env.GOOGLE_API_KEY
-});
-
